4cd0e27a3499e2880fd214995eeca3b3b66e9a8d
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-28 22:17:19 +0100

    setup

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import { tweetsData } from "./data";
+import { tweetsData } from "./data.js";
+
+const tweetBtn = document.getElementById("tweet-btn");
+const tweetInput = document.getElementById("tweet-input");
+const feed = document.getElementById("feed");
+
+tweetBtn.addEventListener("click", function () {
+  console.log(tweetInput.value);
+  tweetInput.value = "";
+});
